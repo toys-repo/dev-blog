a9ece5b0e959fbc7edf688f73a4e069563c5775a
Gosh95 <pocal777@naver.com>
2023-05-16 00:07:33 +0900

    Header 컴포넌트 layout 배치

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,20 @@
 import './globals.css';
 import { Nunito } from 'next/font/google';
 
+import ClientOnly from '@components/ClientOnly';
+import Header from '@components/layout/Header';
+
 const nunito = Nunito({ subsets: ['latin'] });
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <ClientOnly>
+          <Header />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
   );
 };
