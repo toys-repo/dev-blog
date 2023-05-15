b603ce8d1a78b32a39faf29f9c7d74d66228b6ca
Gosh95 <pocal777@naver.com>
2023-05-16 00:17:43 +0900

    Main 컴포넌트 구현

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,17 +4,18 @@ import { Nunito } from 'next/font/google';
 import ClientOnly from '@components/ClientOnly';
 import Header from '@components/layout/Header';
 import Footer from '@components/layout/Footer';
+import Main from '@components/layout/Main';
 
 const nunito = Nunito({ subsets: ['latin'] });
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body className={nunito.className}>
+      <body className={`${nunito.className} w-full min-h-screen flex flex-col`}>
         <ClientOnly>
           <Header />
         </ClientOnly>
-        {children}
+        <Main>{children}</Main>
         <ClientOnly>
           <Footer>© 2023 Gosh95. All Rights Reserved</Footer>
         </ClientOnly>
