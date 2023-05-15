5f60d91232abd53715361aa9468fb5b96e33eb10
Gosh95 <pocal777@naver.com>
2023-05-16 00:08:46 +0900

    Header 컴포넌트 px css 추가

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -32,7 +32,7 @@ const navItems = [
 const Header = () => {
   return (
     <header className="fixed top-0 z-10 w-full h-16 bg-white border-b border-b-neutral-300 shadow-md">
-      <div className="w-full h-full flex items-center justify-between">
+      <div className="xs:px-4 md:px-12 w-full h-full flex items-center justify-between">
         <Logo href="/" label="GOSH95" />
         <div className="w-fit h-full flex items-center justify-between gap-1">
           <nav className="w-fit h-full">
