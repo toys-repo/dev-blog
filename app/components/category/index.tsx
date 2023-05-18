0eb63aa331faebb1e72bda72310cbc4695b789c2
Gosh95 <pocal777@naver.com>
2023-05-18 10:27:01 +0900

    Category 컴포넌트 사이즈 축소

--- app/components/category/index.tsx
+++ app/components/category/index.tsx
@@ -5,7 +5,7 @@ interface CategoryProps {
 
 const Category = ({ name, postCount }: CategoryProps) => {
   return (
-    <div className="px-3 w-fit h-fit text-center cursor-pointer border rounded-xl xs:text-sm md:text-base bg-neutral-200 text-neutral-600 hover:brightness-110 transition">
+    <div className="px-3 w-fit h-fit text-center cursor-pointer border rounded-xl text-sm bg-neutral-200 text-neutral-600 hover:brightness-110 transition">
       {!postCount ? `${name}` : `${name} (${postCount})`}
     </div>
   );
