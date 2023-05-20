66139ed2d38d27e7284845e20004c3ce3bd6dec6
Gosh95 <pocal777@naver.com>
2023-05-20 23:29:33 +0900

    /category 페이지 구현

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,10 +1,41 @@
+import PostService from '@app/services';
+
+import Container from '@components/Container';
+import CategoryList from '@components/category/CategoryList';
+import MessageBox from '@components/MessageBox';
+import PostCardList from '@components/post/PostCardList';
+import LineSeparator from '@components/LineSeparator';
+
 export const metadata = {
   title: 'Category',
   description: 'Category',
 };
 
-const Category = () => {
-  return <div>Category</div>;
+export const getData = async () => {
+  const categories = await PostService.getCategories();
+  const posts = await PostService.getPosts();
+  return {
+    categories,
+    posts,
+  };
+};
+
+const Category = async () => {
+  const { categories, posts } = await getData();
+
+  return (
+    <Container maxWidth="md">
+      {categories.length === 0 ? (
+        <MessageBox message="There is no category." />
+      ) : (
+        <>
+          <CategoryList categories={categories} />
+          <LineSeparator />
+          <PostCardList posts={posts} />
+        </>
+      )}
+    </Container>
+  );
 };
 
 export default Category;
