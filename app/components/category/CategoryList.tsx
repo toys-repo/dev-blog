bb9016925bcbb662a3870cf399341609d58e219f
Gosh95 <pocal777@naver.com>
2023-05-20 18:13:21 +0900

    CategoryList 기본 높이 변경

--- app/components/category/CategoryList.tsx
+++ app/components/category/CategoryList.tsx
@@ -8,7 +8,7 @@ interface CategoryListProps {
 
 const CategoryList = ({ categories }: CategoryListProps) => {
   return (
-    <div className="w-full flex flex-wrap gap-2 xs:h-48 md:h-60 overflow-y-auto">
+    <div className="w-full flex flex-wrap gap-2 h-fit xs:max-h-48 md:max-h-60 overflow-y-auto">
       {categories.map((category) => (
         <Category key={category.name} name={category.name} postCount={category.postCount} />
       ))}
