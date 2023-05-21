0c08bcd77ab9da764726a93291e8146a7b785ab3
Gosh95 <pocal777@naver.com>
2023-05-21 15:42:03 +0900

    Text 컴포넌트 onClick 추가

--- app/components/Text.tsx
+++ app/components/Text.tsx
@@ -5,11 +5,12 @@ import { TextVariant } from '@app/types';
 interface TextProps {
   variant: TextVariant;
   className?: string;
+  onClick?: () => void;
   children: React.ReactNode;
 }
 
-const Text = ({ variant, className, children }: TextProps) => {
-  return React.createElement(variant, { className }, children);
+const Text = ({ variant, className, onClick, children }: TextProps) => {
+  return React.createElement(variant, { className, onClick }, children);
 };
 
 export default Text;
