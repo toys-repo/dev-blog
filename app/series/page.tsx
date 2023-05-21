9ffc49e47a6739b523708a20d3d89f407da9e684
Gosh95 <pocal777@naver.com>
2023-05-21 16:19:04 +0900

    /series 페이지 구현

--- app/series/page.tsx
+++ app/series/page.tsx
@@ -1,10 +1,38 @@
+import BlogService from '@app/services';
+
+import Container from '@components/Container';
+import MessageBox from '@components/MessageBox';
+import SeriesList from '@components/series/SeriesList';
+
 export const metadata = {
   title: 'Series',
   description: 'Series',
 };
 
-const Series = () => {
-  return <div>Series</div>;
+const getSeries = async () => {
+  try {
+    const series = await BlogService.getSeries();
+    return {
+      series,
+    };
+  } catch (e) {
+    return {
+      hasError: true,
+    };
+  }
+};
+
+const Series = async () => {
+  const { series, hasError } = await getSeries();
+
+  return (
+    <Container>
+      {hasError && <MessageBox message="Oops!! Server error is occurred." />}
+      {!hasError &&
+        series &&
+        (series.length === 0 ? <MessageBox message="There is no series." /> : <SeriesList series={series} />)}
+    </Container>
+  );
 };
 
 export default Series;
